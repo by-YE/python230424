d4b9fe72a49cef78d652b03733d367ef7ec80494
by-YE <boyoungye@gmail.com>
2023-04-25 10:03:10 +0900

    분기구문연습1

--- demo5_day2.py
+++ demo5_day2.py
@@ -1,1 +1,14 @@
 # demo5_day2.py
+# ifelse.py
+
+score = int(input("점수를 입력:"))
+if 90 <= score <= 100:
+    grade = "A"
+elif 80 <= score < 90:
+    grade = "B"
+elif 70 <= score < 80:
+    grade = "C"
+else:
+    grade = "D"
+
+print("등급은 ", grade)
